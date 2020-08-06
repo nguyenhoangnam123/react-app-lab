f5bdf385be94ead2f8a1c487ca91d17473568ada
namnh <namnh21894@gmail.com>
2020-08-06 14:58:10 +0700

    fix repo and testView useMaster

--- src/views/TestView/TestRepository.ts
+++ src/views/TestView/TestRepository.ts
@@ -1,20 +1,26 @@
 import { Observable, defer } from "rxjs";
-import { map } from 'rxjs/operators';
-import axios from 'axios';
+import { map } from "rxjs/operators";
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: '/rpc/dms/app-user',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
+  baseURL: "/rpc/dms/app-user",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 export class TestRepository<T> {
-    public getUser = (): Observable<T[]> => {
-        return defer(()=> axiosInstance.post('/list', { skip: 0, take: 10 }))
-            .pipe(map(result => result.data));
-    };
+  public getUser = (): Observable<T[]> => {
+    return defer(() => axiosInstance.post("/list", { skip: 0, take: 10 })).pipe(
+      map((result) => result.data),
+    );
+  };
+  public getTotalUser = (): Observable<number> => {
+    return defer(() =>
+      axiosInstance.post("/count", { skip: 0, take: 10 }),
+    ).pipe(map((result) => result.data));
+  };
 }
 
 export const repository = new TestRepository<any>();
--- src/views/TestView/TestView.tsx
+++ src/views/TestView/TestView.tsx
@@ -1,100 +1,99 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Observable, Subject, forkJoin } from 'rxjs';
-import { take, takeUntil, tap } from 'rxjs/operators';
-import { repository } from './TestRepository';
+import React, { useEffect, useState } from "react";
+import { forkJoin, Observable, Subject } from "rxjs";
+import { take, takeUntil, tap } from "rxjs/operators";
+import { repository } from "./TestRepository";
 
-export default function TestView(){
-    const {users} = useFetch();
-    return (<>
-        <ul>
-            {users && users.length > 0 && users.map((user: any) => (
-                <li key={user.id}>{user.name}</li>
-            ))}
-        </ul>
-    </>)
+export default function TestView() {
+  const { list, total } = useMaster(
+    repository.getUser(),
+    repository.getTotalUser(),
+  );
+
+  return (
+    <>
+      <h1>
+        total: <span>{total}</span>
+      </h1>
+      <ul>
+        {list &&
+          list.length > 0 &&
+          list.map((user: any) => <li key={user.id}>{user.displayName}</li>)}
+      </ul>
+    </>
+  );
 }
 
-function useMaster<T>(
-    getList: Observable<T[]>,
-    getTotal: Observable<number>,
-) {
-    const isCurrent = useRef<boolean>(true);
-    const [list, setList] = useState<T[]>([]);
-    const [total, setTotal] = useState<number>(0);
-    const [loadList, setLoadList] = useState<boolean>(true);
-    const [loading, setLoading] = useState<boolean>(false);
-    const {isCancelled, cancelSubcription } = subcriptionCancellation();
+function useMaster<T>(getList: Observable<T[]>, getTotal: Observable<number>) {
+  const [list, setList] = useState<T[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const [loadList, setLoadList] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { isCancelled, cancelSubcription } = subcriptionCancellation();
 
-    useEffect(() => {
-      if(loadList){
-        forkJoin([getList, getTotal])
+  useEffect(() => {
+    if (loadList) {
+      forkJoin([getList, getTotal])
         .pipe(
-            tap(() => setLoading(true)),
-            takeUntil(isCancelled),
-            take(1),
+          tap(() => setLoading(true)),
+          takeUntil(isCancelled),
+          take(1),
         )
-        .subscribe({
-            next: (results: [T[], number]) => {
-                console.log('list ', results[0]);
-                console.log('total ', results[1]);
-                if(isCurrent.current){
-                    setList([...results[0]]);
-                    setTotal(results[1]);
-                }
-            },
-            complete: () => {
-                setLoading(false);
-                setLoadList(false);
-            },
-
-        })
-      }
-      return () => {
-        isCurrent.current = false;
-        cancelSubcription();
+        .subscribe((results: [T[], number]) => {
+          setLoadList(false);
+          setLoading(false);
+          setList([...results[0]]);
+          setTotal(results[1]);
+        });
     }
-    }, [cancelSubcription, getList, getTotal, isCancelled, loadList]);
+    return () => {
+      cancelSubcription();
+    };
+  }, [cancelSubcription, getList, getTotal, isCancelled, loadList]);
 
-    return {list, total, loading}
+  return { list, total, loading };
 }
 
-function useFetch<T>(): {
-    users: T[]
-}{
-    const [users, setUsers] = useState<T[]>([]);
-    const {isCancelled, cancelSubcription } = subcriptionCancellation();
+// function useFetch<T>(): {
+//   users: T[];
+// } {
+//   const [users, setUsers] = useState<T[]>([]);
+//   const { isCancelled, cancelSubcription } = subcriptionCancellation();
 
-    useEffect(() => {
-        // subcribe it, create subject to init notification when get success,
-        repository.getUser().pipe(
-            takeUntil(isCancelled), // complete when isCancelled emit a value
-            take(1), // emit only the first count value from source
-        ).subscribe((data: T[]) => {
-            setUsers(data);
-        })
-        return () => {
-            cancelSubcription();
-        }
-    }, [cancelSubcription, isCancelled]);
+//   useEffect(() => {
+//     // subcribe it, create subject to init notification when get success,
+//     repository
+//       .getUser()
+//       .pipe(
+//         takeUntil(isCancelled), // complete when isCancelled emit a value
+//         take(1), // emit only the first count value from source
+//       )
+//       .subscribe((data: T[]) => {
+//         setUsers(data);
+//       });
+//     return () => {
+//       cancelSubcription();
+//     };
+//   }, [cancelSubcription, isCancelled]);
 
-    return {
-        users 
-    }
+//   return {
+//     users,
+//   };
+// }
+
+/* expose stop subject as Observable and next method to init stop */
+
+function subcriptionCancellation(): {
+  isCancelled: Observable<void>;
+  cancelSubcription: () => void;
+} {
+  // define subject to cancel all subscriptions
+  const stop$ = new Subject<void>();
+  const cancelSubcription = (): void => {
+    stop$.next();
+    stop$.complete();
+  };
+  return {
+    isCancelled: stop$.asObservable(),
+    cancelSubcription,
+  };
 }
-
-/* expose stop subject as Observable and next method to init stop */ 
-function subcriptionCancellation(): {isCancelled: Observable<void>, cancelSubcription: () => void}{
-    // define subject to cancel all subscriptions
-    const stop$ = new Subject<void>();
-    const cancelSubcription = (): void => {
-        stop$.next();
-        stop$.complete();
-    }
-    return {
-        isCancelled: stop$.asObservable(),
-        cancelSubcription
-    }
-}
-
-
-
